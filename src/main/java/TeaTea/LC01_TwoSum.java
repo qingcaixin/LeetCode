e2b6981b289006d42dd6a80bcbc608330eae0c4a
wangxin <1292490331@qq.com>
2021-06-08 20:01:28 +0800

    面试题TreedLocalMap

--- src/main/java/TeaTea/LC01_TwoSum.java
+++ src/main/java/TeaTea/LC01_TwoSum.java
@@ -9,24 +9,24 @@ import java.util.Map;
  */
 public class LC01_TwoSum {
 
-    public int[] twoSum(int[] nums, int target){
+    public int[] twoSum(int[] nums, int target) {
         int len = nums.length;
-        Map<Integer,Integer> hashMap = new HashMap<>(len - 1);
-        hashMap.put(nums[0],0);
-        for (int i = 1; i < len; i++){
+        Map<Integer, Integer> hashMap = new HashMap<>(len - 1);
+        hashMap.put(nums[0], 0);
+        for (int i = 1; i < len; i++) {
             int another = target - nums[i];
-            if (hashMap.containsKey(another)){
-                return new int[]{i,hashMap.get(another)};
+            if (hashMap.containsKey(another)) {
+                return new int[]{i, hashMap.get(another)};
             }
-            hashMap.put(nums[i],i);
+            hashMap.put(nums[i], i);
         }
         throw new IllegalArgumentException("NO two sum soluation");
     }
 
     public static void main(String[] args) {
-        int[] nums = {1,2,3};
+        int[] nums = {1, 2, 3};
         int target = 0;
         LC01_TwoSum a = new LC01_TwoSum();
-        a.twoSum(nums,target);
+        a.twoSum(nums, target);
     }
 }
