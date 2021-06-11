6a68f4b2dbecc3c42ad9006d58473266a91aa3cd
wangxin <1292490331@qq.com>
2021-06-11 20:42:14 +0800

    整数反转

---
+++ src/main/java/TeaTea/LC07_ReverseInt.java
@@ -0,0 +1,30 @@
+package TeaTea;
+
+/**
+ * @author wangxin
+ * @data 2021/6/11 20:32
+ */
+public class LC07_ReverseInt {
+
+    public int reverse(int x){
+        int res = 0;
+        while (x!=0){
+            //每次取末尾数字
+            int tmp = x % 10;
+            //判断是否大于最大32位整数
+            if (res > 214748364 || (res == 214748364 && tmp > 7)){
+                return 0;
+            }
+
+            //判断是否小于 最小32位整数
+            if (res< -214748364 || (res == -214748364 && tmp < -8)){
+                return 0;
+            }
+
+            res = res * 10 + tmp;
+            x /= 10;
+        }
+
+        return res;
+    }
+}
